1cce21b18658fe77efe3a337403a94832be5a5a7
santhoshkumar-038 <naarojusanthoshkumar038@gmail.com>
2024-03-10 13:10:43 +0530

    initial commit

---
+++ email-service/email-service/src/test/java/com/santhosh/eventdrivenmicroservices/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.eventdrivenmicroservices.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
